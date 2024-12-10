07bdc077b10ea4b9c01411ea4908a65a96f608f6
Jonathan <jonacedev@gmail.com>
2024-12-10 20:10:11 +0100

    Initial Commit

---
+++ Tripe/App/TripeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripeApp.swift
+//  Tripe
+//
+//  Created by Jonathan Miguel Onrubia Solis on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TripeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashView()
+        }
+    }
+}
