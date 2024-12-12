fd75825e9aa814ce106c1801ba18a16c4c9bd2e0
Mei De Blas <114696563+hdevmei@users.noreply.github.com>
2024-12-12 12:20:26 +0100

    Feature/firebase config (#1)
    * Add firebase connection

--- Tripe/App/AppDelegate.swift
+++ Tripe/App/AppDelegate.swift
@@ -7,12 +7,14 @@
 
 
 import UIKit
+import FirebaseCore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        FirebaseApp.configure()
         return true
     }
 }
